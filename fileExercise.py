e19b6300a4a16c983375e181be1e070bd611c1ea
Praise <ipadpraise@gmail.com>
2022-04-22 14:37:36 -0400

    file processing exercises

---
+++ fileExercise.py
@@ -0,0 +1,15 @@
+
+
+while True: 
+    try:
+        fileName = input("Enter File Name: ")
+        handle = open(fileName)
+    except:
+        print("File does not exist")
+    else:
+        count = 0
+        for i in handle:
+            count += 1
+            i = i.strip()
+            print(count, i)
+        break
--- stringer.py
+++
@@ -1,8 +0,0 @@
-fruit = 'Monty Python'
-index = 0
-
-
-for i in fruit:
-    if index <= 3:
-        print(i)
-    index+=1
