5e518b1c301bc4f606f1fba01f118cd45c5247ba
Praise <ipadpraise@gmail.com>
2022-04-22 17:41:41 -0400

    list changes

---
+++ romeo.py
@@ -0,0 +1,10 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    words = line.rstrip().split()
+    for i in words:
+        if i not in lst:
+            lst.append(i)
+lst.sort()
+print(lst)
