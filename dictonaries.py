bfe94cc226b33ffba97c76ae613672e238206a0d
Praise <ipadpraise@gmail.com>
2022-04-23 04:25:32 -0400

    dict exercises

---
+++ dictonaries.py
@@ -0,0 +1,32 @@
+"""
+months = {}
+
+months = {1: "Jan", 2: "Feb", 3:"Mar", 4:"April", 5: "May"}
+
+rand = {"Amy": 1, "Suzuki":4, "Cello":10, "Fanta": -1}
+
+del(months[1])
+print(months)
+print(months.keys())
+
+months[1] = "Jan"
+print(months)
+
+for i,j in months.items():
+    print(i,j)
+"""
+
+line = input("Enter Line: ")
+dictCount = {}
+count = 0
+
+words = line.split()
+print("Words: ", words)
+
+for word in words:
+    if word not in dictCount.keys():
+        dictCount[word] = dictCount.get(word,1)
+    else:
+        dictCount[word] = dictCount.get(word) + 1
+
+print(dictCount)
--- romeo.py
+++ romeo.py
@@ -1,4 +1,23 @@
 fname = input("Enter file name: ")
+handle = open(fname)
+count = 0 
+for line in handle:
+    if line.startswith('From'):
+        line = line.strip()
+        count += 1
+        splitLine = line.split()
+        address = splitLine[1]
+        print(address)
+
+
+print("There were", count, "lines in the file with From as the first word")
+
+
+
+
+
+
+"""
 fh = open(fname)
 lst = list()
 for line in fh:
@@ -8,3 +27,4 @@ for line in fh:
             lst.append(i)
 lst.sort()
 print(lst)
+"""
